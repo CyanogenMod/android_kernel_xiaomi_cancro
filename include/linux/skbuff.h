01805b074d6d5b68769ffe4dc5b408d24fb7fcab
Sabrina Dubroca <sd@queasysnail.net>
2015-10-30 15:46:36 +0100

    net: add length argument to skb_copy_and_csum_datagram_iovec
    Without this length argument, we can read past the end of the iovec in
    memcpy_toiovec because we have no way of knowing the total length of the
    iovec's buffers.
    
    This is needed for stable kernels where 89c22d8c3b27 ("net: Fix skb
    csum races when peeking") has been backported but that don't have the
    ioviter conversion, which is almost all the stable trees <= 3.18.
    
    This also fixes a kernel crash for NFS servers when the client uses
     -onfsvers=3,proto=udp to mount the export.
    
    Change-Id: I1865e3d7a1faee42a5008a9ad58c4d3323ea4bab
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Reviewed-by: Hannes Frederic Sowa <hannes@stressinduktion.org>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -325,7 +325,7 @@ typedef unsigned char *sk_buff_data_t;
 #define NET_SKBUFF_NF_DEFRAG_NEEDED 1
 #endif
 
-/** 
+/**
  *	struct sk_buff - socket buffer
  *	@next: Next buffer in list
  *	@prev: Previous buffer in list
@@ -519,7 +519,7 @@ struct sk_buff {
  */
 static inline struct dst_entry *skb_dst(const struct sk_buff *skb)
 {
-	/* If refdst was not refcounted, check we still are in a 
+	/* If refdst was not refcounted, check we still are in a
 	 * rcu_read_lock section
 	 */
 	WARN_ON((skb->_skb_refdst & SKB_DST_NOREF) &&
@@ -1932,7 +1932,7 @@ static inline int skb_cow_head(struct sk_buff *skb, unsigned int headroom)
  *	is untouched. Otherwise it is extended. Returns zero on
  *	success. The skb is freed on error.
  */
- 
+
 static inline int skb_padto(struct sk_buff *skb, unsigned int len)
 {
 	unsigned int size = skb->len;
@@ -2105,7 +2105,8 @@ extern int	       skb_copy_datagram_iovec(const struct sk_buff *from,
 					       int size);
 extern int	       skb_copy_and_csum_datagram_iovec(struct sk_buff *skb,
 							int hlen,
-							struct iovec *iov);
+							struct iovec *iov,
+							int len);
 extern int	       skb_copy_datagram_from_iovec(struct sk_buff *skb,
 						    int offset,
 						    const struct iovec *from,
