da2bd67ea7ebc42225214e9145606c65f3b95d4e
Samir Mehta <samirn@codeaurora.org>
2015-12-08 00:18:14 +0800

    ARM: msm: GCC Version change.
    - Changing gcc toolchain to 4.9 version.
    - Fix linker error of unknown CPU architecture by
      adding CONFIG_MSM_CORTEX_A7 in the arch options
      for the GCC. The issue is seen after bump up to
      gcc-4.9.
    - Fix section mismatch errors after upgrading gcc to 4.9
      version. To fix these errors variables are moved to
      appropriate sections.
    
    Change-Id: I7b5c9212e63826a0cb5b7e4ab161737ae87a2157
    Signed-off-by: Samir Mehta <samirn@codeaurora.org>

--- arch/arm/mach-msm/krait-regulator.c
+++ arch/arm/mach-msm/krait-regulator.c
@@ -1398,7 +1398,7 @@ static int __devexit krait_power_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id krait_power_match_table[] = {
+static struct of_device_id krait_power_match_table[] __initdata = {
 	{ .compatible = "qcom,krait-regulator", },
 	{}
 };
@@ -1413,7 +1413,7 @@ static struct platform_driver krait_power_driver = {
 	},
 };
 
-static struct of_device_id krait_pdn_match_table[] = {
+static struct of_device_id krait_pdn_match_table[] __initdata = {
 	{ .compatible = "qcom,krait-pdn", },
 	{}
 };
