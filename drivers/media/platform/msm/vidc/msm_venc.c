d9063237f4c28ed83d9f75b4db2eb0e14a3da69e
jrizzoli <joey@cyanogenmoditalia.it>
2015-12-15 18:25:23 +0100

    vidc: replace VB2_MAX_FRAME with VIDEO_MAX_FRAME
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- drivers/media/platform/msm/vidc/msm_venc.c
+++ drivers/media/platform/msm/vidc/msm_venc.c
@@ -951,12 +951,12 @@ static int msm_venc_queue_setup(struct vb2_queue *q,
 		}
 
 		if (*num_buffers < MIN_NUM_CAPTURE_BUFFERS ||
-				*num_buffers > VB2_MAX_FRAME) {
+				*num_buffers > VIDEO_MAX_FRAME) {
 			int temp = *num_buffers;
 
 			*num_buffers = clamp_val(*num_buffers,
 					MIN_NUM_CAPTURE_BUFFERS,
-					VB2_MAX_FRAME);
+					VIDEO_MAX_FRAME);
 			dprintk(VIDC_INFO,
 				"Changing buffer count on CAPTURE_MPLANE from %d to %d for best effort encoding\n",
 				temp, *num_buffers);
