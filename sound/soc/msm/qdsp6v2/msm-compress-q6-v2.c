d688cae99614f6341a759fe4c00d80eaeb3ec1fd
Ravi Kumar Alamanda <ralama@codeaurora.org>
2016-04-12 17:32:54 -0700

    ASoC: msm: audio-effects: fix stack overread and heap overwrite
    Fix overwrite of updt_params allocated in heap, and stack overread
    where param pointer is passed from user space.
    
    Bug: 27555224
    Change-Id: I024ea8c716c9c8b61e901346442ef4671d1a8910
    Signed-off-by: Ravi Kumar Alamanda <arkumar@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
  * Copyright (C) 2015 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -1894,7 +1894,7 @@ static int msm_compr_audio_effects_config_info(struct snd_kcontrol *kcontrol,
 					       struct snd_ctl_elem_info *uinfo)
 {
 	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
-	uinfo->count = 128;
+	uinfo->count = MAX_PP_PARAMS_SZ;
 	uinfo->value.integer.min = 0;
 	uinfo->value.integer.max = 0xFFFFFFFF;
 	return 0;
